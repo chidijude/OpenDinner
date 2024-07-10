52a4960cec6e617b8691ff14bca38477a95fb038
Chidi Jude <cjude@appzonegroup.com>
2024-07-10 19:06:18 +0100

    rename aggregate folders

--- OpenDinner.Domain/Common/ValueObjects/Rating.cs
+++ OpenDinner.Domain/Common/ValueObjects/Rating.cs
@@ -1,9 +1,4 @@
 ﻿using OpenDinner.Domain.Common.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenDinner.Domain.Common.ValueObjects
 {
@@ -15,7 +10,7 @@ namespace OpenDinner.Domain.Common.ValueObjects
             Value = value;
         }
 
-        public static Rating CreateUnique(double rating = 0) => new( rating);
+        public static Rating CreateUnique(double rating = 0) => new(rating);
 
         public override IEnumerable<object> GetEqualityComponents()
         {
